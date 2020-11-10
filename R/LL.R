9deee783e86a4d0f85ec225f030f6b284c4485c6
David Brown <ndbrown6@gmail.com>
2020-11-10 15:52:41 -0500

    Adds likelihood function

--- R/LL.R
+++ R/LL.R
@@ -1,5 +1,9 @@
-'LL' <- function(p, n, p_hat, a_hat, b_hat, log = TRUE)
+'LL' <- function(p, n, a_hat, b_hat)
 {
-	
+	ll = 1
+	x = c(rep(0, round((1-p)*n)), rep(1, round(p*n)))
+	for (i in 1:n) {
+		ll = ll * exp(lgamma(x[i]+a_hat) + lgamma(1-x[i]+b_hat) - lgamma(a_hat+b_hat+1) - lbeta(a_hat, b_hat))
+	}
 	return(invisible(ll))
 }
