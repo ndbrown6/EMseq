6eb4f6b7ed9c47c71f7f06ceea8d393d31420219
David Brown <ndbrown6@gmail.com>
2020-11-10 23:30:25 -0500

    Update LL.R

--- R/LL.R
+++ R/LL.R
@@ -1,9 +1,9 @@
 'LL' <- function(p, n, a_hat, b_hat)
 {
 	ll = 1
-	x = c(rep(0, round((1-p)*n)), rep(1, round(p*n)))
-	for (i in 1:n) {
-		ll = ll * exp(lgamma(x[i]+a_hat) + lgamma(1-x[i]+b_hat) - lgamma(a_hat+b_hat+1) - lbeta(a_hat, b_hat))
+	r = c(rep(0, round((1-p)*n)), rep(1, round(p*n)))
+	for (j in 1:n) {
+		ll = ll * exp(lgamma(r[j]+a_hat) + lgamma(1-r[j]+b_hat) - lgamma(a_hat+b_hat+1) - lbeta(a_hat, b_hat))
 	}
 	return(invisible(ll))
 }
