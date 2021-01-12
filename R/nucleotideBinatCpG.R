bba690281e4584f75214ddd149f5ff084ddf19c9
David Brown <ndbrown6@gmail.com>
2021-01-11 19:41:44 -0500

    Update nucleotideBinatCpG.R

--- R/nucleotideBinatCpG.R
+++ R/nucleotideBinatCpG.R
@@ -1,22 +1,12 @@
 'nucleotideBinatCpG' <- function(pileup, mode = "TCC")
 {
 	if (mode == "TCC") {
-		# BS-seq and EM-seq
 		ctmat = matrix(NA, nrow=nrow(pileup$seqs), ncol=ncol(pileup$seqs)-1)
 		ctmat[pileup$seqs[,-1,drop=FALSE]=="C"] = 0
 		ctmat[pileup$seqs[,-1,drop=FALSE]=="T"] = 1
 		rownames(ctmat) = pileup$seq[,"read_name"]
 		colnames(ctmat) = colnames(pileup$seq)[-1]
-	} else if (mode == "CTT") {
-		# TAP-seq
-		
-	} else if (mode == "TTC") {
-		# TAB-seq
-		
-	} else if (mode == "CCT") {
-		# CAP-seq
-		
 	}
-
+	
 	return(invisible(ctmat))
 }
