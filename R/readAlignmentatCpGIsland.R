fe8410d576df59ea1e64595a26a8d82882076745
David Brown <ndbrown6@gmail.com>
2021-01-12 18:42:40 -0500

    Update readAlignmentatCpGIsland.R

--- R/readAlignmentatCpGIsland.R
+++ R/readAlignmentatCpGIsland.R
@@ -1,6 +1,6 @@
 'readAlignmentatCpGIsland' <- function(bam, bai, chromosome, start, end, strand)
 {
-	url_bam = BamFile(file = bam, index = bai, asMates = TRUE)
+	bam_url = BamFile(file = bam, index = bai, asMates = TRUE)
 	cpg_island = GRanges(seqnames = chromosome, ranges = IRanges(start, end))
 	scan_flags = scanBamFlag(isPaired = EMseq::.EMenv$isPaired, isProperPair = EMseq::.EMenv$isProperPair, isUnmappedQuery = EMseq::.EMenv$isUnmappedQuery, 
 				 hasUnmappedMate = EMseq::.EMenv$hasUnmappedMate, isMinusStrand = EMseq::.EMenv$isMinusStrand,
@@ -9,21 +9,14 @@
 				 isNotPassingQualityControls = EMseq::.EMenv$isNotPassingQualityControls, isDuplicate = EMseq::.EMenv$isDuplicate,
 				 isSupplementaryAlignment = EMseq::.EMenv$isSupplementaryAlignment)
 	what = c("seq", "qual")
-	metadata = readGAlignmentPairs(file = url_bam,
-				       use.names = TRUE,
-				       strandMode = 1,
-				       param = ScanBamParam(which = cpg_island, flag = scan_flags))
-	seq_quals = readGAlignments(file = url_bam,
-				    use.names = TRUE,
-				    param = ScanBamParam(which = cpg_island, flag = scan_flags, what = what))
-	
+	metadata = readGAlignmentPairs(file = bam_url, use.names = TRUE, strandMode = 1, param = ScanBamParam(which = cpg_island, flag = scan_flags))
+	seq_quals = readGAlignments(file = bam_url, use.names = TRUE, param = ScanBamParam(which = cpg_island, flag = scan_flags, what = what))
 	seq_quals = seq_quals[names(seq_quals) %in% names(metadata)[data.frame(metadata)[,"strand.first"] == strand],]
 	metadata = metadata[unique(names(seq_quals)),]
-	
-	stacked_seqs = stackStringsFromGAlignments(x = seq_quals, region = cpg_island, what = what[1])
-	stacked_quals = stackStringsFromGAlignments(x = seq_quals, region = cpg_island, what = what[2])
+	seqs = stackStringsFromGAlignments(x = seq_quals, region = cpg_island, what = what[1])
+	quals = stackStringsFromGAlignments(x = seq_quals, region = cpg_island, what = what[2])
 	
 	return(invisible(list(metadata = metadata,
-			      seqs = stacked_seqs,
-			      quals = stacked_quals)))
+			      seqs = seqs,
+			      quals = quals)))
 }
