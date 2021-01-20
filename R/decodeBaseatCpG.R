4a8d94ed1d535bebe527aa61f4eef2997c936950
David Brown <ndbrown6@gmail.com>
2021-01-19 20:57:43 -0500

    Update decodeBaseatCpG.R

--- R/decodeBaseatCpG.R
+++ R/decodeBaseatCpG.R
@@ -2,8 +2,8 @@
 {
 	decoded = matrix(NA, nrow = nrow(alignment$seqs), ncol = ncol(alignment$seqs)-1)
 	if (mode == "TCC") {
-		decoded[alignment$seqs[,-1,drop=FALSE] == "C"] = 1
-		decoded[alignment$seqs[,-1,drop=FALSE] == "T"] = 0
+		decoded[alignment$seqs[,-1,drop=FALSE] == "C"] = 0
+		decoded[alignment$seqs[,-1,drop=FALSE] == "T"] = 1
 		rownames(decoded) = alignment$seq[,"read_name"]
 		colnames(decoded) = colnames(alignment$seq)[-1]
 	}
